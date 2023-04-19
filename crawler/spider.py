79b0bdf61df8e03a099ca15539217ca1f443dde8
Xukay101 <zaca03zaca@gmail.com>
2023-04-19 18:27:38 -0300

    Added parse and download system

--- crawler/spider.py
+++ crawler/spider.py
@@ -1,4 +1,7 @@
 from config import DEPTH_LIMIT
+from .downloader import downloader
+from .parser import parse_html
+from .storage import storage
 
 class Spider:
     def __init__(self, start_urls):
@@ -13,5 +16,13 @@ class Spider:
             if url in self.visited_urls:
                 continue
             self.visited_urls.add(url)
+            # Download
+            html = downloader(url)
+            # Parse
+            new_urls, data = parse_html(html)
+            print(data['word_freq'])
+            # Storage
+
+            self.urls += new_urls
         
         self.run(depth+1)
