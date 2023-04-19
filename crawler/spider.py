3c0da0f5c6d2e1aa488adfcc0d0de34834435928
Xukay101 <zaca03zaca@gmail.com>
2023-04-19 19:28:35 -0300

    Added topic keyword system

--- crawler/spider.py
+++ crawler/spider.py
@@ -1,6 +1,6 @@
 import sys, signal
 
-from config import DEPTH_LIMIT
+from config import DEPTH_LIMIT, KEYWORDS, MIN_KEYWORD_OCCURRENCES
 from .downloader import downloader
 from .parser import parse_html
 from .storage import storage_content, storage_urls
@@ -9,6 +9,7 @@ class Spider:
     def __init__(self, start_urls):
         self.urls = start_urls
         self.visited_urls = set()
+        self.storage_urls = []
 
         self.data = []
 
@@ -28,7 +29,14 @@ class Spider:
             # Parse
             new_urls, data = parse_html(html)
             # Storage
-            self.data.append(data)
+            if KEYWORDS:
+                cant = sum(data['keyword_freq'].values())
+                if cant >= MIN_KEYWORD_OCCURRENCES:
+                    self.data.append(data)
+                    self.storage_urls.append(url)
+            else:
+                self.data.append(data)
+                self.storage_urls.append(url)
 
             self.urls += new_urls
         
@@ -36,5 +44,5 @@ class Spider:
 
     def end(self, signal, frame):
         storage_content(self.data)
-        storage_urls(self.visited_urls)
+        storage_urls(self.storage_urls)
         sys.exit(0)
