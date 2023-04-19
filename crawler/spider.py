5753bdbd73aa718623a5ad71f482c00f7392687b
Xukay101 <zaca03zaca@gmail.com>
2023-04-19 19:09:05 -0300

    Added storage system

--- crawler/spider.py
+++ crawler/spider.py
@@ -1,12 +1,19 @@
+import sys, signal
+
 from config import DEPTH_LIMIT
 from .downloader import downloader
 from .parser import parse_html
-from .storage import storage
+from .storage import storage_content, storage_urls
 
 class Spider:
     def __init__(self, start_urls):
         self.urls = start_urls
         self.visited_urls = set()
+
+        self.data = []
+
+        # Señal de interrupcion (Ctrl+C)
+        signal.signal(signal.SIGINT, self.end)
 
     def run(self, depth=0):
         if depth > DEPTH_LIMIT:
@@ -20,9 +27,14 @@ class Spider:
             html = downloader(url)
             # Parse
             new_urls, data = parse_html(html)
-            print(data['word_freq'])
             # Storage
+            self.data.append(data)
 
             self.urls += new_urls
         
         self.run(depth+1)
+
+    def end(self, signal, frame):
+        storage_content(self.data)
+        storage_urls(self.visited_urls)
+        sys.exit(0)
